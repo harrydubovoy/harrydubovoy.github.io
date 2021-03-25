56ec94395c35cb50e0c00694ee04c88e340e3ec2
harrydubovoy <harrydubovoy@gmail.com>
2021-03-25 20:59:20 +0200

    add faceapp

---
+++ faceapp/javascript/project.js
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  const globalDocument = document;
+  const global = window;
+
+  // sliders
+  const splideOptions = {
+    type: 'loop',
+    autoHeight: true,
+    arrows: true,
+    perPage: 3,
+    gap: 30,
+
+    breakpoints: {
+      576: {
+        perPage: 1,
+        arrows: false,
+      },
+      768: {
+        perPage: 2,
+        arrows: false,
+        gap: 15,
+      },
+      1024: {
+        arrows: false,
+      }
+    }
+  }
+
+  new Splide('.js-advisors-advisors', splideOptions).mount();
+
+  new Splide('.js-advisors-press', splideOptions).mount();
+
+  const header = globalDocument.querySelector('#header');
+  const menuButton = header.querySelector('#menu-button');
+  const navigationGroup = header.querySelector('#navigation-group');
+  const navigation = header.querySelector('#navigation');
+
+  // show logo
+  if (global.innerWidth > 1023) {
+    const headerLogo = header.querySelector('#header-logo');
+    globalDocument.addEventListener('scroll', function (event) {
+      if (global.scrollY > 100) {
+        headerLogo.classList.add('is-visible');
+      } else {
+        headerLogo.classList.remove('is-visible');
+      }
+    })
+  }
+
+  function handleNavigation (classAction) {
+    menuButton.classList[classAction]('is-active');
+    navigationGroup.classList[classAction]('is-active');
+    globalDocument.body.classList[classAction]('is-freeze');
+  }
+
+  // mobile menu
+  menuButton.addEventListener('click', function (event) {
+    handleNavigation('toggle');
+  })
+
+  // smooth scroll
+  navigation.addEventListener('click', function (event) {
+    if (event.target.tagName === 'A') {
+      handleNavigation('remove');
+      const anchor = globalDocument.querySelector(event.target.hash);
+      anchor.scrollIntoView({ behavior: 'smooth' });
+    }
+  })
+
+
+  console.log( "ready!" );
+});
